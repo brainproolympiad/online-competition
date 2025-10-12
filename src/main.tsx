15e2735ee03baba584a279d636664582247ce020
brainproolympiad <brainproolympiad@gmail.com>
2025-10-13 00:00:37 +0100

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-// index.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/online-competition">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
