e349bd91a346bd3725d1238c03b01d6963614328
Brain Pro <brainproolympiad@gmail.com>
2025-10-13 12:09:39 +0100

    Deploy update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker"; // 👈 add this
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/online-competition/", // repo name exactly as on GitHub
+  plugins: [
+    react(),
+    checker({ typescript: false }) // 👈 disables TypeScript type checking
+  ],
+  base: "/online-competition/", // 👈 your GitHub repo name
+
+  build: {
+    target: "esnext",
+    sourcemap: false,
+    minify: true,
+  },
 });
